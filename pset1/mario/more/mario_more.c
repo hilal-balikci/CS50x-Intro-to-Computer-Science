cbb9d367f996a2fa0652153922a18ea330da238e
hilal-balikci <76621251+hilal-balikci@users.noreply.github.com>
2021-06-01 21:36:43 +0300

    Create mario_more.c

---
+++ pset1/mario/more/mario_more.c
@@ -0,0 +1,45 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void pyramid(int n);
+
+int main(void)
+{
+    int height = 0;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (!(height >= 1 && height <= 8));
+    
+    pyramid(height);
+}
+
+//printing empty spaces
+void space(int l)
+{
+    for (int k = 0; k < l; k++)
+    {
+        printf(" ");
+    }    
+}
+
+void pyramid(int n)
+{
+    for (int i = 0; i < n; i++)
+    { 
+        space(n - i - 1);
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        } 
+        
+        space(2)
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        } 
+        
+        printf("\n");
+    }
+}
